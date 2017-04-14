d8ba506af392d68ce62b1fbc6fdbd36263645e96
Kathryn Anne <djkatscan@gmail.com>
2017-04-14 15:04:45 -0400

    Minor changes to timing of notes

--- YourSong.js
+++ YourSong.js
@@ -1,24 +1,22 @@
-/*
-Elton John/Moulin Rouge  - Your Song
-
-i hope you don't mind, i hope you don't mind 
-that i put down in words.
-how wonderful life is, now you're in the world.
-
-*/
+// Elton John/Moulin Rouge  - Your Song
+//
+// I hope you don't mind, I hope you don't mind that I
+// put down in words. how wonderful life is, now
+// you're in the world.
+//
 basic.forever(() => {
     images.iconImage(IconNames.Heart).showImage(0)
     images.iconImage(IconNames.SmallHeart).showImage(0)
 })
 music.playTone(466, music.beat(BeatFraction.Half))
 music.playTone(494, music.beat(BeatFraction.Half))
-music.playTone(466, music.beat(BeatFraction.Quarter))
+music.playTone(466, music.beat(BeatFraction.Half))
 music.playTone(494, music.beat(BeatFraction.Half))
 music.playTone(587, music.beat(BeatFraction.Whole))
 music.rest(music.beat(BeatFraction.Whole))
 music.playTone(466, music.beat(BeatFraction.Half))
 music.playTone(494, music.beat(BeatFraction.Half))
-music.playTone(466, music.beat(BeatFraction.Quarter))
+music.playTone(466, music.beat(BeatFraction.Half))
 music.playTone(494, music.beat(BeatFraction.Half))
 music.playTone(587, music.beat(BeatFraction.Whole))
 music.rest(music.beat(BeatFraction.Whole))
@@ -31,8 +29,8 @@ music.playTone(494, music.beat(BeatFraction.Double))
 music.rest(music.beat(BeatFraction.Double))
 music.playTone(494, music.beat(BeatFraction.Whole))
 music.playTone(466, music.beat(BeatFraction.Whole))
-music.playTone(392, music.beat(BeatFraction.Half))
-music.playTone(311, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(311, music.beat(BeatFraction.Half))
 music.playTone(349, music.beat(BeatFraction.Whole))
 music.playTone(262, music.beat(BeatFraction.Double))
 music.rest(music.beat(BeatFraction.Whole))
